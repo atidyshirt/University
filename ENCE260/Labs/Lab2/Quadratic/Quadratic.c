7beebae9f3f841259ae9543c95587d2dca129939
Jordan Pyott <jordanpyott@gmail.com>
2020-07-29 14:00:06 +1200

    Updating lab quiz - July 29

--- ENCE260/Labs/Lab2/Quadratic/Quadratic.c
+++ ENCE260/Labs/Lab2/Quadratic/Quadratic.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include <math.h>
 
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
 int main(void)
 {
-    int a = 0;
-    int b = 0;
-    int c = 0;
-
-    scanf("%i %i %i", &a, &b, &c);
-
-    printf("%i %i %i", a, b, c);
+    float a = 0;
+    float b = 0;
+    float c = 0;
+    scanf("%f %f %f", &a, &b, &c);
+    float ans_1 = (-b + sqrt(b*b - 4 * a * c)) / 2 * a;
+    float ans_2 = (-b - sqrt(b*b - 4 * a * c)) / 2 * a; 
+    if (b*b - 4 * a * c < 0) {
+        printf("Complex roots");
+    } else if (a == 0) {
+        printf("Not a quadratic");
+    } else {
+        printf("Roots are %.4f and %.4f", MIN(ans_1, ans_2), MAX(ans_1, ans_2));
+    }
 }
