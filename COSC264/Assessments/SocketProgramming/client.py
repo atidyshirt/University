9010ff46f7d40fba5a709f17ba9d19b9d8cd17f2
Jordan Pyott <jordanpyott@gmail.com>
2020-08-12 20:21:42 +1200

    Updating Assignment - August 12th

--- COSC264/Assessments/SocketProgramming/client.py
+++ COSC264/Assessments/SocketProgramming/client.py
@@ -1,31 +1,58 @@
 import socket
 
 HEADERSIZE = 5
+
+# These are the inputs
 IP = socket.gethostbyname('localhost')
 PORT = 1024
+DATE = 'date' # or time
+
+def checkInputs(DATE, IP, PORT):
+    if not IP:
+        return "The IP address is not specified"
+    if PORT < 1024 and PORT > 64000:
+        return "The Port number is not within specified range (1024, 64000)"
+    if DATE != 'date' or DATE != 'time':
+        return "Make sure the client asks for the date or time"
+
+
+def format_request(Date):
+    """
+    Formats the packet into the desired format
+    """
+    MagicNo = 0x497E
+    PacketType = 0x0001
+    if Date == 'date':
+        RequestType = 0x0001
+    elif Date == 'time':
+        RequestType = 0x0002
+    bytelist = []
+    bytelist.append(MagicNo.to_bytes(2, 'big'))
+    bytelist.append(PacketType.to_bytes(2, 'big'))
+    bytelist.append(RequestType.to_bytes(2, 'big'))
+    arrayBytes = bytearray()
+    for x in bytelist:
+        arrayBytes += x
+    return arrayBytes
+
+request_packet = format_request(DATE)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # set to grab IPv4 and socket_stream is to create TCP protocols
+
+#  s.settimeout(1)
 s.connect((IP, PORT)) # This is the port number (1024)
+s.sendto(request_packet, (IP, PORT))
 
 # To terminate the client in a loop
 complete_message = ""
 
-new_msg = True
 while True:
     msg = s.recv(48) # this is just a message with the number of bytes alocated to each packet (if we lower this, we must use a loop to get all the data)
 
     if len(msg) <= 0:
         break
 
-    if new_msg:
-        msglen = int(msg[:HEADERSIZE])
-        new_msg = False
-
     complete_message += msg.decode("utf-8")
 
-    if len(complete_message) - HEADERSIZE == msglen:
-        print(complete_message[HEADERSIZE:])
-        new_msg = True
-        full_message = ""
-
 print(complete_message)
+s.close()
--- COSC264/Assessments/SocketProgramming/server.py
+++ COSC264/Assessments/SocketProgramming/server.py
@@ -2,32 +2,60 @@ import socket
 import select
 import datetime
 
-HEADERSIZE = 5
+header = 0
 IP = socket.gethostbyname('localhost')
-PORT = 1024
+PORT_english = 1024
+PORT_maori = 1025
+PORT_german = 1026
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # set to grab IPv4 and socket_stream is to create TCP protocols
+s_english = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # set to grab IPv4 and socket_stream is to create TCP protocols
+s_maori = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # set to grab IPv4 and socket_stream is to create TCP protocols
+s_german = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # set to grab IPv4 and socket_stream is to create TCP protocols
+
+server_sockets = [[s_english, PORT_english], [s_maori, PORT_maori], [s_german, PORT_german]]
 
 # This will allow us to reuse the same port number instead of having to bump up
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-s.bind((IP, PORT))
-s.listen(48)
+for sock, port in server_sockets:
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((IP, port))
+    sock.listen(48)
 
 # SERVERSIDE FUNCTIONS
+def packetCheck(packet):
+    """
+    checks the integrity of the packet, and makes sure that it is formatted correctly
+    """
+    print(packet)
+    MagicNo = packet[:2]
+    PacketType = packet[2:4]
+    RequestType = packet[:-2]
+    if MagicNo != 0x497E:
+        return False
+    if PacketType != 0x0001:
+        return False
+    if RequestType != 0x0001 or RequestType != 0x0002:
+        return False
+    return True
+
 def getDate():
     """
-    grabs the current date using datetime, 
-    and returns it as a string.
+    grabs the current date using datetime and returns it as a string.
     """
     date = str(datetime.datetime.utcnow())
     date = date.split(" ")
     return date[0]
 
 while True: # while there is a connection
-    clt, adr=s.accept() # As we established above, accept returns the connection and address (these are being assigned to variables)
-    print(f"Connection to {adr} established")
-    msg = getDate()
-    msg = f'{len(msg):<{HEADERSIZE}}' + msg 
-
-    clt.send(bytes(msg, "utf-8"))
+    for s, port in server_sockets:
+        clt, adr=s.accept() # As we established above, accept returns the connection and address (these are being assigned to variables)
+        print(f"Connection to {adr} established")
+        packet, source = clt.recvfrom(48)
+        print(f"{packetCheck(packet)}")
+        if packetCheck(packet):
+            msg = getDate()
+            clt.send(bytes(msg, "utf-8"))
+            clt.close()
+        else:
+            msg = "Packet is not correct"
+            clt.send(bytes(msg, "utf-8"))
+            clt.close()
