e7723c717c0b6069cdebbc1693ba5209882c3209
Jordan Pyott <jordanpyott@gmail.com>
2020-08-13 15:58:02 +1200

    Updating assessments - August 13th

--- COSC264/Assessments/SocketProgramming/client.py
+++ COSC264/Assessments/SocketProgramming/client.py
@@ -1,56 +1,143 @@
 import socket
+import argparse
 
+def start_client(DATE, HOST, PORT):
 # These are the inputs
-IP = socket.gethostbyname('localhost')
-PORT = 1024
-DATE = 'date' # or time
+    IP = socket.gethostbyname(HOST)
 
-def checkInputs(DATE, IP, PORT):
-    if not IP:
-        return "The IP address is not specified"
-    if PORT < 1024 and PORT > 64000:
-        return "The Port number is not within specified range (1024, 64000)"
-    if DATE != 'date' or DATE != 'time':
-        return "Make sure the client asks for the date or time"
+    def checkInputs(DATE, IP, PORT):
+        if not IP:
+            print("The IP address is not specified")
+            return False
+        if PORT < 1024 or PORT > 64000:
+            print("The Port number is not within specified range (1024:64000)")
+            return False
+        if DATE != 'date' and DATE != 'time':
+            print("MSG parameter must be set to `date` or `time`")
+            return False
+        return True
+
+    def decrypt_message(packet):
+        info = [packet[i:i+1] for i in range(0, len(packet), 1)]
+        if len(info) < 13:
+            print("Packet does not include minimum headersize")
+            return -1
+
+        MagicNo = int.from_bytes(info[0] + info[1], 'big')
+        
+        if MagicNo != 0x497E:
+            print("MagicNo is incorrect: `{}` recieved, must equal `0x497E`".format(MagicNo))
+            return -1
+        PacketType = int.from_bytes(info[2] + info[3], 'big')
+        if PacketType != 0x0002:
+            print("PacketType is incorrect: `{}` received, must equal `0x0002`".format(PacketType))
+        LanguageCode = int.from_bytes(info[4] + info[5], 'big')
+        if LanguageCode < 0x0001 or LanguageCode > 0x0003:
+            print("LanguageCode is incorrect: `{}` received, must be within range (1, 3)".format(LanguageCode))
+            return -1
+        Year = int.from_bytes(info[6] + info[7], 'big')
+        if Year > 2100:
+            print("Year is incorrect: `{}` received, must be below 2100".format(Year))
+            return -1
+        Month = int.from_bytes(info[8], 'big')
+        if Month < 1 or Month > 12:
+            print("Month is incorrect: `{}` received, must be between 1 and 12".format(Month))
+            return -1
+        Day = int.from_bytes(info[9], 'big')
+        if Day < 1 or Day > 31:
+            print("Day is incorrect: `{}` received, must be between 1 and 31".format(Day))
+            return -1
+        Hour = int.from_bytes(info[10], 'big')
+        if Hour < 0 or Hour > 23:
+            print("Hour is incorrect: `{}` received, must be within range (0, 23)".format(Hour))
+            return -1
+        Minute = int.from_bytes(info[11], 'big')
+        if Minute < 0 or Minute > 59:
+            print("Minute is incorrect: `{}` received, must be within range (0, 59)".format(Minute))
+            return -1
+        Length = int.from_bytes(info[12], 'big')
+        text = bytearray()
+        for i in range(13, len(info)):
+            text += info[i]
+        text = text.decode('utf-8')
+
+        if len(info) != 13 + Length:
+            print("Length of packet does not match packet received")
+            return -1
+
+        return text
+
+    def format_request(Date):
+        """
+        Formats the packet into the desired format
+        """
+        MagicNo = 0x497E
+        PacketType = 0x0001
+        if Date == 'date':
+            RequestType = 0x0001
+        elif Date == 'time':
+            RequestType = 0x0002
+        else:
+            return -1
+        bytelist = []
+        bytelist.append(MagicNo.to_bytes(2, 'big'))
+        bytelist.append(PacketType.to_bytes(2, 'big'))
+        bytelist.append(RequestType.to_bytes(2, 'big'))
+        arrayBytes = bytearray()
+        for x in bytelist:
+            arrayBytes += x
+        return arrayBytes
+
+    if checkInputs(DATE, IP, PORT):
+        request_packet = format_request(DATE)
+        if request_packet == -1:
+            print("The `date` parameter must be set to either `date` or `time`")
+
+        else:
+
+                s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # set to grab IPv4 and socket_stream is to create TCP protocols
+                s.settimeout(1)
+                s.sendto(request_packet, (IP, PORT))
+
+                complete_message = bytearray()
+
+                while True:
+                    #  read, write, exception = select.select(sockets, [], [])
+
+                    try:
+                        msg, source = s.recvfrom(1024)
+
+                        if len(msg) <= 0:
+                            break
+
+                        complete_message += msg
+
+                    except socket.timeout:
+                        print("Client socket timeout")
+                        break
+
+                    except socket.error:
+                        print("Client socket timeout")
+                        break
+
+                    result = decrypt_message(complete_message)
+                    if result != -1:
+                        print(result)
+                        break
+                    s.close()
 
 
-def format_request(Date):
-    """
-    Formats the packet into the desired format
-    """
-    MagicNo = 0x497E
-    PacketType = 0x0001
-    if Date == 'date':
-        RequestType = 0x0001
-    elif Date == 'time':
-        RequestType = 0x0002
-    bytelist = []
-    bytelist.append(MagicNo.to_bytes(2, 'big'))
-    bytelist.append(PacketType.to_bytes(2, 'big'))
-    bytelist.append(RequestType.to_bytes(2, 'big'))
-    arrayBytes = bytearray()
-    for x in bytelist:
-        arrayBytes += x
-    return arrayBytes
+def Main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("MSG", help="The message to receive from server must be `date` or `time`", type=str)
+    parser.add_argument("HOST", help="The Hostname to connect to", type=str)
+    parser.add_argument("PORT", help="The Port number to connect to", type=int)
 
-request_packet = format_request(DATE)
+    args = parser.parse_args()
+    start_client(args.MSG, args.HOST, args.PORT)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # set to grab IPv4 and socket_stream is to create TCP protocols
+if __name__ == "__main__":
+    Main()
 
-#  s.settimeout(1)
-s.connect((IP, PORT)) # This is the port number (1024)
-s.sendto(request_packet, (IP, PORT))
 
-# To terminate the client in a loop
-complete_message = ""
 
-while True:
-    msg = s.recv(48) # this is just a message with the number of bytes alocated to each packet (if we lower this, we must use a loop to get all the data)
-
-    if len(msg) <= 0:
-        break
-
-    complete_message += msg.decode("utf-8")
-
-print(complete_message)
-s.close()
--- COSC264/Assessments/SocketProgramming/server.py
+++ COSC264/Assessments/SocketProgramming/server.py
@@ -1,79 +1,186 @@
 import socket
+import select
+import argparse
 import datetime
 
-IP = socket.gethostbyname('localhost')
-PORT_english = 1024
-PORT_maori = 1025
-PORT_german = 1026
-
-s_english = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # set to grab IPv4 and socket_stream is to create TCP protocols
-s_maori = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # set to grab IPv4 and socket_stream is to create TCP protocols
-s_german = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # set to grab IPv4 and socket_stream is to create TCP protocols
-
-server_sockets = [[s_english, PORT_english], [s_maori, PORT_maori], [s_german, PORT_german]]
-
-# This will allow us to reuse the same port number instead of having to bump up
-for sock, port in server_sockets:
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind((IP, port))
-    sock.listen(48)
-
+def start_server(PORT_english, PORT_maori, PORT_german):
 # SERVERSIDE FUNCTIONS
-def packetCheck(packet):
-    """
-    checks the integrity of the packet, and makes sure that it is formatted correctly
-    """
-    info = [packet[i:i+2] for i in range(0, len(packet), 2)]
-    MagicNo = int.from_bytes(info[0], 'big')
-    PacketType = int.from_bytes(info[1], 'big')
-    RequestType = int.from_bytes(info[2], 'big')
-    if MagicNo != 0x497E:
-        print("Magic wrong")
-    if PacketType != 0x0001:
-        return False
-    if RequestType != 0x0001 and RequestType != 0x0002:
-        return False
-    return True
+    def check_port(PORT):
+        if PORT < 1024 or PORT > 64000:
+            print("The Port number is not within specified range (1024:64000)")
+            return False
+        return True
 
-def checkRequestType(packet):
-    info = [packet[i:i+2] for i in range(0, len(packet), 2)]
-    RequestType = int.from_bytes(info[2], 'big')
-    if RequestType == 0x0001:
-        return 'date'
-    else:
-        return 'time'
+    def packetCheck(packet):
+        """
+        checks the integrity of the packet, and makes sure that it is formatted correctly
+        """
+        info = [packet[i:i+2] for i in range(0, len(packet), 2)]
+        MagicNo = int.from_bytes(info[0], 'big')
+        PacketType = int.from_bytes(info[1], 'big')
+        RequestType = int.from_bytes(info[2], 'big')
+        if MagicNo != 0x497E:
+            print("Magic wrong")
+        if PacketType != 0x0001:
+            return False
+        if RequestType != 0x0001 and RequestType != 0x0002:
+            return False
+        return True
 
-def getDate():
-    """
-    grabs the current date using datetime and returns it as a string.
-    """
-    date = str(datetime.datetime.utcnow())
-    date = date.split(" ")
-    return date[0]
-
-def getTime():
-    """
-    grabs the current date using datetime and returns it as a string.
-    """
-    date = str(datetime.datetime.utcnow())
-    date = date.split(" ")
-    return date[0]
-
-while True: # while there is a connection
-    for s, port in server_sockets:
-        clt, adr=s.accept() # As we established above, accept returns the connection and address (these are being assigned to variables)
-        print(f"Connection to {adr} established")
-        packet, source = clt.recvfrom(48)
-        if packetCheck(packet):
-            if checkRequestType(packet) == 'date':
-                msg = getDate()
-                clt.send(bytes(msg, "utf-8"))
-                clt.close()
-            elif checkRequestType(packet) == 'time':
-                msg = getTime()
-                clt.send(bytes(msg, "utf-8"))
-                clt.close()
+    def checkRequestType(packet):
+        info = [packet[i:i+2] for i in range(0, len(packet), 2)]
+        RequestType = int.from_bytes(info[2], 'big')
+        if RequestType == 0x0001:
+            return 'date'
+        elif RequestType == 0x0002:
+            return 'time'
         else:
-            msg = "Packet is not correct"
-            clt.send(bytes(msg, "utf-8"))
-            clt.close()
+            return -1
+
+    def getDate(sock):
+        """
+        grabs the current date using datetime and returns it as a string.
+        """
+        months = {
+            'english': ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+            'maori': ["Kohitatea", "Hui-tanguru", "Poutu ̄-te-rangi", "Paenga-whawha", "Haratua", "Pipiri", "Hongongoi", "Here-turi-koka", "Mahuru", "Whiringa-a-nuku", "Whiringa-a-rangi", "Hakihea"],
+            'german': ["Januar", "Februar", "Marz", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"]
+        }
+
+        MagicNo = 0x497E.to_bytes(2, 'big')
+        PacketType = 0x0002.to_bytes(2, 'big')
+        if sock is s_english:
+            LanguageCode = 0x0001
+            flag = 'english'
+        elif sock is s_maori:
+            LanguageCode = 0x0002
+            flag = 'maori'
+        elif sock is s_german:
+            LanguageCode = 0x0003
+            flag = 'german'
+        date = datetime.datetime.today()
+        LanguageCode = LanguageCode.to_bytes(2, 'big')
+        year = date.year.to_bytes(2, 'big')
+        language_months = months[flag]
+        chosen_month = language_months[(date.month)]
+        month = date.month.to_bytes(1, 'big')
+        day = date.day.to_bytes(1, 'big')
+        hour = date.hour.to_bytes(1, 'big')
+        minute = date.minute.to_bytes(1, 'big')
+        if flag == 'english':
+            text = "Today's date is {} {}, {}".format(chosen_month, date.day, date.year)
+        elif flag == 'maori':
+            text = "Ko te ra o tenei ra ko {} {}, {}".format(chosen_month, date.day, date.year)
+        else:
+            text = "Heute ist der {} {}, {}".format(chosen_month, date.day, date.year)
+
+        lengthNow = len(text)
+        length = lengthNow.to_bytes(1, 'big')
+
+        bytelist = [MagicNo, PacketType, LanguageCode, year, month, day, hour, minute, length]
+
+        out = bytearray()
+
+        for byteset in bytelist:
+            out += byteset
+
+        out.extend(text.encode("utf-8"))
+
+        return out
+
+    def getTime(sock):
+        """
+        grabs the current time using datetime and returns it as a string.
+        """
+        MagicNo = 0x497E.to_bytes(2, 'big')
+        PacketType = 0x0002.to_bytes(2, 'big')
+        if sock is s_english:
+            LanguageCode = 0x0001
+            flag = 'english'
+        elif sock is s_maori:
+            LanguageCode = 0x0002
+            flag = 'maori'
+        elif sock is s_german:
+            LanguageCode = 0x0003
+            flag = 'german'
+        date = datetime.datetime.today()
+        LanguageCode = LanguageCode.to_bytes(2, 'big')
+        year = date.year.to_bytes(2, 'big')
+        month = date.month.to_bytes(1, 'big')
+        day = date.day.to_bytes(1, 'big')
+        hour = date.hour.to_bytes(1, 'big')
+        minute = date.minute.to_bytes(1, 'big')
+        if flag == 'english':
+            text = f"The current time is {date.hour}:{date.minute}"
+        elif flag == 'maori':
+            text = f"Ko te wa o tenei wa {date.hour}:{date.minute}"
+        else:
+            text = f"Die Uhrzeit ist {date.hour}:{date.minute}"
+
+        lengthNow = len(text)
+        length = lengthNow.to_bytes(1, 'big')
+
+        bytelist = [MagicNo, PacketType, LanguageCode, year, month, day, hour, minute, length]
+
+        out = bytearray()
+
+        for byteset in bytelist:
+            out += byteset
+
+        out.extend(text.encode('utf-8'))
+
+        return out
+
+
+    # Server setup
+    IP = socket.gethostbyname('localhost')
+
+    s_english = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # set to grab IPv4 and socket_stream is to create TCP protocols
+    s_maori = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # set to grab IPv4 and socket_stream is to create TCP protocols
+    s_german = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # set to grab IPv4 and socket_stream is to create TCP protocols
+
+    server_sockets = [[s_english, PORT_english], [s_maori, PORT_maori], [s_german, PORT_german]]
+
+    for sock, port in server_sockets:
+        if check_port(port):
+            sock.bind((IP, port))
+        else:
+            print("Server closed: port {} isnt within range (1024 - 64000)".format(port))
+            return -1
+
+    sockets = [s_english, s_maori, s_german]
+
+    while True: # while there is a connection
+
+        read, write, exception = select.select(sockets, [], [])
+        for s in read:
+            packet, source = s.recvfrom(48)
+            print(f"Packet recieved from {source}")
+            if packetCheck(packet):
+                if checkRequestType(packet) == 'date':
+                    msg = getDate(s)
+                    s.sendto(msg, source)
+                elif checkRequestType(packet) == 'time':
+                    msg = getTime(s)
+                    s.sendto(msg, source)
+                else:
+                    msg = "Packet discarded"
+                    msg = msg.encode("utf-8")
+                    s.sendto(bytes(msg))
+            else:
+                msg = "Packet discarded"
+                msg = msg.encode("utf-8")
+                s.sendto(bytes(msg))
+                s.close()
+
+def Main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("PORT_English", help="The Port number to grab date/time in English.", type=int)
+    parser.add_argument("PORT_Maori", help="The Port number to grab date/time in Te Aro Maori.", type=int)
+    parser.add_argument("PORT_German", help="The port number to grab date/time in German.", type=int)
+
+    args = parser.parse_args()
+    start_server(args.PORT_English, args.PORT_Maori, args.PORT_German)
+
+if __name__ == "__main__":
+    Main()
