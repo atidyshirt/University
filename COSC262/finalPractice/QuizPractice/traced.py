e6b2f461b388c5f84043523edfa0af612a022e7d
Jordan Pyott <jordanpyott@gmail.com>
2020-09-23 12:23:04 +1200

    Updating for a test

--- COSC262/finalPractice/QuizPractice/traced.py
+++ COSC262/finalPractice/QuizPractice/traced.py
@@ -1,14 +1,5 @@
 from traces import adjacency_list, dfs, dfs_loop
-g = """\
-U 8 W
-1 3 2
-0 6 7
-2 5 5
-2 7 1
-4 6 2
-5 7 3
-"""
-graph = adjacency_list(g)
-print(dfs(graph, 5))
 
+g = {{}}
 
+print(dijkstras(graph, 5))
