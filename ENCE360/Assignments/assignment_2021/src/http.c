1506ff48c78b6094218ccb680fc0991f55be2bcf
Jordan Pyott <jordanpyott@gmail.com>
2021-10-04 13:17:12 +1300

    Updating assignment, completed Q1, fixed up notes

--- ENCE360/Assignments/assignment_2021/src/http.c
+++ ENCE360/Assignments/assignment_2021/src/http.c
@@ -13,15 +13,20 @@
 
 Buffer* initilize_buffer(int size) {
   /* initilizes a new buffer of length 0
-  * @return Buffer* -- new buffer
+  * @return Buffer* buffer -- pointer to a new buffer
   */
-  Buffer* buffer = (Buffer *)malloc(sizeof(Buffer));
-  buffer->data = (char *)malloc((sizeof(char)) * size);
+  Buffer* buffer = malloc(sizeof(Buffer));
+  buffer->data = malloc((sizeof(char)) * size);
   buffer->length = size;
   return buffer;
 }
 
 Buffer* append_buffer(Buffer* source, Buffer* extend) {
+  /* append a buffer two another buffer
+  * @param Buffer* source  -- the buffer to append data to
+  * @param Buffer* edtend  -- the buffer to append
+  * @return Buffer* source -- pointer the resulting buffer
+  */
   source->length += extend->length;
   source->data = realloc(source->data, source->length);
   source->data = strncat(source->data, extend->data, source->length);
@@ -30,7 +35,7 @@ Buffer* append_buffer(Buffer* source, Buffer* extend) {
 
 void free_buffer(Buffer* buffer) {
   /* free memory associated with a given buffer
-  * @param Buffer* -- buffer to free
+  * @param Buffer* buffer -- buffer to free
   */
   free(buffer->data);
   free(buffer);
@@ -38,13 +43,15 @@ void free_buffer(Buffer* buffer) {
 
 int initilize_socket(char* host, int port) {
   /* Initilizes new TCP socket
-  * @param char* -- hostname to connect to
-  * @param int -- port to connect on
-  * @return int -- file descriptor of new socket
+  * @param char* host -- hostname to connect to
+  * @param int port   -- port to connect on
+  * @return int fd    -- file descriptor of new socket
   */
-
   int rc;
   int their_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+  char port_char[20];
+  sprintf(port_char, "%d", port); // set port to char for getaddrinfo
 
   // set address information
   struct addrinfo their_addrinfo;
@@ -52,24 +59,25 @@ int initilize_socket(char* host, int port) {
   memset(&their_addrinfo, 0, sizeof(their_addrinfo));
   their_addrinfo.ai_family = AF_INET;
   their_addrinfo.ai_socktype = SOCK_STREAM;
-  getaddrinfo(host, NULL, &their_addrinfo, &our_addrinfo);
+
+  getaddrinfo(host, port_char, &their_addrinfo, &our_addrinfo);
 
   // connect to socket and error check
   rc = connect(their_sockfd, our_addrinfo->ai_addr, our_addrinfo->ai_addrlen);
   if (rc == -1) {
     perror("Failed to connect"); exit(1);
-    freeaddrinfo(our_addrinfo); // free address info
   }
 
+  freeaddrinfo(our_addrinfo); // free address info
   return their_sockfd;
 }
 
 Buffer* http_query(char *host, char *page, int port) {
   /* makes an http 1.0 query to a given website and returns response string
-  * @param char* -- hostname of website
-  * @param char* -- page to request
-  * @param int   -- port to connect on
-  * @return Buffer* -- request response in buffer
+  * @param char* host      -- hostname of website
+  * @param char* page      -- page to request
+  * @param int port        -- port to connect on
+  * @return Buffer* buffer -- request response in buffer
   */
 
   int stream_length = 0;
@@ -78,18 +86,18 @@ Buffer* http_query(char *host, char *page, int port) {
   Buffer* buffer = initilize_buffer(0);
 
   snprintf(send_line, BUF_SIZE,
-    "GET %s HTTP/1.0\r\n"
+    "GET /%s HTTP/1.0\r\n"
     "Host: %s\r\n"
     "User-Agent: getter\r\n\r\n", page, host
   );
 
+
   if (write(fd, send_line, strlen(send_line)) >= 0) {
     while ((stream_length = read(fd, recieve_line, BUF_SIZE)) > 0) {
-      recieve_line[stream_length] = '\0';
       Buffer* stream_buffer = initilize_buffer(stream_length);
+      recieve_line[stream_length] = '\0';
       stream_buffer->data = recieve_line;
       append_buffer(buffer, stream_buffer);
-      free_buffer(stream_buffer);
     }
   }
   return buffer;
